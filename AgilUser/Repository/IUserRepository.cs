4a067b822a106665356500242974a6ddaef94bd0
AwesomeBromonus <Emilbro@outlook.dk>
2026-03-03 10:45:03 +0100

    Iuser

--- AgilUser/Repository/IUserRepository.cs
+++ AgilUser/Repository/IUserRepository.cs
@@ -1,6 +1,11 @@
-namespace AgilUser.Repository;
+using UsersApi.Models;
 
-public class IUserRepository
+namespace UsersApi.Repositories;
+
+public interface IUserRepository
 {
-    
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<List<User>> GetAllAsync();
+    Task AddAsync(User user);
 }
